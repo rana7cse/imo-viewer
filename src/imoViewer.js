3f0f8268fea4182dc3d3c5d46123b1e43d3fac08
Rana <rana7cse@gmail.com>
2019-09-09 00:16:19 +0600

    seperating these files

--- src/imoViewer.js
+++ src/imoViewer.js
@@ -17,47 +17,78 @@
   /*
   * Error codes
   * */
-  const IMAGE_SIZE_NOT_MATCH = 400;
-  const TOO_LARGE_FILE = 500;
+  const INVALID_FILE_FORMAT = 1;
+  const INVALID_FILE_SIZE = 2;
+  const INPUT_ERROR = 3;
+  const INVALID_IMAGE_RESULATION = 4;
 
   const fireEvent = (fn,...params) => {
     return (typeof fn === 'function') && fn(...params);
   };
 
   $.fn.imoViewer = function (options) {
-    const nameOnly = options.nameOnly || false;
-    const maxWidth = options.maxWidth || null;
-    const maxHeight = options.maxHeight || null;
+    const baseElems = $(this);
+
+    const config = $.extend({}, {
+      'image' : {
+        'minHeight' : null,
+        'minWidth'  : null,
+        'maxHeight' : null,
+        'maxWidth'  : null
+      },
+      'allowFiles' : '*',
+      'maxSize' : '*', // Kilobyte
+      'multiple' : false
+    }, options);
+
+    let isValidExtension = (file, supportedExtensions) => {
+      let extension = ((file instanceof File ? ( file.name || '' ) : file).split('.').pop()) || null;
+      return (supportedExtensions === '*') || (extension !== null && supportedExtensions.indexOf(extension) >= 0);
+    };
+
+    let isValidSize = (file, maxSize) => file instanceof File ? (file.size / 1024) <= maxSize : false;
+
+    let runFileViewer = (file, elem) => {
+
+      if (!isValidSize(file,config.maxSize))  {
+        fireEvent(config.onError,INVALID_FILE_SIZE,elem,file);
+        throw Error("Invalid file size");
+      }
+
+      if (!isValidExtension(file,config.allowFiles))  {
+        fireEvent(config.onError,INVALID_FILE_FORMAT,elem,file);
+        throw Error("Invalid file format");
+      }
+
+      const reader = new FileReader();
+      reader.onload = function(file) {
+        const image = new Image();
+        const content = file.target.result;
+        image.src = content;
+
+        image.onload = function (img) {
+          const height = this.height;
+          const width = this.width;
+          if (( maxWidth != null && height > maxHeight) || (maxHeight != null && width > maxWidth)) {
+            fireEvent(options.onError,IMAGE_SIZE_NOT_MATCH,element,this);
+            $(element).val("");
+          } else {
+            fireEvent(options.onBeforePreview,element,file.target,this);
+            $(options.preview).attr('src',content);
+            fireEvent(options.onAfterPreview,element,file.target,this);
+            fireEvent(options.onSuccess,element,file.target,this);
+          }
+        }
+      };
+      reader.readAsDataURL(file);
+    };
 
     $(this).change(function (e) {
       e.preventDefault();
-      const file = $(this)[0].files[0];
-      const element = $(this);
-      if (nameOnly){
-        $(options.preview).html(file.name);
-      } else {
-        const reader = new FileReader();
-        reader.onload = function(file) {
-          const image = new Image();
-          const content = file.target.result;
-          image.src = content;
-
-          image.onload = function (img) {
-            const height = this.height;
-            const width = this.width;
-            if (( maxWidth != null && height > maxHeight) || (maxHeight != null && width > maxWidth)) {
-              fireEvent(options.onError,IMAGE_SIZE_NOT_MATCH,element,this);
-              $(element).val("");
-            } else {
-              fireEvent(options.onBeforePreview,element,file.target,this);
-              $(options.preview).attr('src',content);
-              fireEvent(options.onAfterPreview,element,file.target,this);
-              fireEvent(options.onSuccess,element,file.target,this);
-            }
-          }
-        };
-        reader.readAsDataURL(file);
-      }
+      let file = $(this)[0].files;
+      let element = $(this);
+      console.log(file);
     })
   };
+
 })($,window,document);
