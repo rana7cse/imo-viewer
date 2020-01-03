6e9f9df96489ac950fe8340624db99a696314acd
Salauddin Rana <rana7cse@gmail.com>
2020-01-03 17:17:51 +0600

    file size validation added

--- src/imoViewer.js
+++ src/imoViewer.js
@@ -17,40 +17,74 @@
   /*
   * Error codes
   * */
-  const INVALID_FILE_FORMAT = 1;
-  const INVALID_FILE_SIZE = 2;
-  const INPUT_ERROR = 3;
-  const INVALID_IMAGE_RESULATION = 4;
+  const INVALID_FILE_FORMAT = 5001;
+  const INVALID_FILE_SIZE = 5002;
+  const INPUT_ERROR = 5003;
+  const INVALID_IMAGE_RESULATION = 5004;
+
+  let defaultConfigurations = {
+    'image' : {
+      'minHeight' : null,
+      'minWidth'  : null,
+      'maxHeight' : null,
+      'maxWidth'  : null
+    },
+    'allowFiles' : '*',
+    'fileSize' : {
+      max : null,
+      min : null,
+    },
+    'multiple' : false
+  };
+
+  const blank = (val) => {
+    return typeof val == 'undefined' || val === null || val === 0 || val === '' || val.length === 0;
+  };
 
   const fireEvent = (fn,...params) => {
     return (typeof fn === 'function') && fn(...params);
   };
 
+  let isValidExtension = (file, supportedExtensions) => {
+    let extension = ((file instanceof File ? ( file.name || '' ) : file).split('.').pop()) || null;
+    return (supportedExtensions === '*') || (extension !== null && supportedExtensions.indexOf(extension) >= 0);
+  };
+
+  let isValidSize = (file, optionFileSize) => {
+    if (!file instanceof File) {
+      throw new Error("File object expected");
+    }
+
+    if (blank(optionFileSize)) {
+      return true;
+    }
+
+    if (typeof optionFileSize !== 'object') {
+      throw new Error("fileSize option should be an object");
+    }
+
+    if (!blank(optionFileSize.min) && file.length < optionFileSize.min) {
+      throw new Error("File size should be bigger then min file size");
+    }
+
+    if (!blank(optionFileSize.max) && file.length > optionFileSize.max) {
+      throw new Error("File size should be less then max file size");
+    }
+
+    return true;
+  };
+
+
+
   $.fn.imoViewer = function (options) {
     const baseElems = $(this);
 
-    const config = $.extend({}, {
-      'image' : {
-        'minHeight' : null,
-        'minWidth'  : null,
-        'maxHeight' : null,
-        'maxWidth'  : null
-      },
-      'allowFiles' : '*',
-      'maxSize' : '*', // Kilobyte
-      'multiple' : false
-    }, options);
+    const config = $.extend({}, defaultConfigurations , options);
 
-    let isValidExtension = (file, supportedExtensions) => {
-      let extension = ((file instanceof File ? ( file.name || '' ) : file).split('.').pop()) || null;
-      return (supportedExtensions === '*') || (extension !== null && supportedExtensions.indexOf(extension) >= 0);
-    };
-
-    let isValidSize = (file, maxSize) => file instanceof File ? (file.size / 1024) <= maxSize : false;
 
     let runFileViewer = (file, elem) => {
 
-      if (!isValidSize(file,config.maxSize))  {
+      if (!isValidSize(file,config.fileSize))  {
         fireEvent(config.onError,INVALID_FILE_SIZE,elem,file);
         throw Error("Invalid file size");
       }
@@ -71,7 +105,6 @@
           const width = this.width;
           if (( maxWidth != null && height > maxHeight) || (maxHeight != null && width > maxWidth)) {
             fireEvent(options.onError,IMAGE_SIZE_NOT_MATCH,element,this);
-            $(element).val("");
           } else {
             fireEvent(options.onBeforePreview,element,file.target,this);
             $(options.preview).attr('src',content);
